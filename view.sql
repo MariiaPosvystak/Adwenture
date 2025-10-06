498d45c978753542a7c0cbee8e113a6d0f2638d5
Mariia Posvystak <mariiaposvystak@gmail.com>
2025-10-06 12:09:48 +0300

    Lisa view mis näed Employees ja Sales Territory Region

---
+++ view.sql
@@ -0,0 +1,53 @@
+Select * from DimEmployee
+Select * from DimSalesTerritory
+
+--lisa view, mis näed EmployeeKey, firstname, 
+--salary, gender, sales territory Region
+
+create view vWEmployeesBySalesTerritory
+as
+Select EmployeeKey, FirstName, Salary, 
+Gender,SalesTerritoryRegion
+from DimEmployee de
+join DimSalesTerritory dst
+on de.SalesTerritoryKey= dst.SalesTerritoryKey 
+--käivitama view
+select * from vWEmployeesBySalesTerritory
+
+--lisa view, mis näed EmployeeKey, firstname, 
+--salary, gender, sales territory Region, 
+--kus Sales Territory Region on NA 
+create view vWNASalesTerritory_Employees
+as
+Select EmployeeKey, FirstName, Salary, 
+Gender,SalesTerritoryRegion
+from DimEmployee de
+join DimSalesTerritory dst
+on de.SalesTerritoryKey= dst.SalesTerritoryKey 
+where dst.SalesTerritoryRegion = 'NA' 
+--käivitama view
+select * from vWNASalesTerritory_Employees
+
+--lisa view, mis näed EmployeeKey, firstname, 
+--gender, sales territory Region 
+create view vWEmployeesNonConfidentialData
+as
+Select EmployeeKey, FirstName,
+Gender,SalesTerritoryRegion
+from DimEmployee de
+join DimSalesTerritory dst
+on de.SalesTerritoryKey= dst.SalesTerritoryKey 
+--käivitama view
+select * from vWEmployeesNonConfidentialData
+
+--lisa view, mis näed sales territory Region, arv empoyees
+create view vWEmployeesCountBySalesTerritory
+as
+Select SalesTerritoryRegion, count(EmployeeKey) as TotalEmployees
+from DimEmployee de
+join DimSalesTerritory dst
+on de.SalesTerritoryKey= dst.SalesTerritoryKey 
+group By SalesTerritoryRegion
+--käivitama view
+select * from vWEmployeesCountBySalesTerritory
+
