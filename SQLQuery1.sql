8b4f774f8833a751d38951f27e2a611a0d5273c2
Mariia Posvystak <mariiaposvystak@gmail.com>
2025-10-06 10:10:27 +0300

    Lisa  ajutised tabelid

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -55,3 +55,14 @@ return (select FirstName from dbo.DimEmployee where EmployeeKey=@id)
 end;
 
 --34
+--Lisa ajutised tabelid
+CREATE TABLE #PersonDetails(Id INT, Name NVARCHAR(20));
+insert into #PersonDetails values(1, 'Mike');
+insert into #PersonDetails values(2, 'John');
+insert into #PersonDetails values(3, 'Todd');
+
+select * from #PersonDetails
+
+--local temporary tabel on loodud?
+select name from tempdb..sysobjects
+where name like '#PersonDetails%'
