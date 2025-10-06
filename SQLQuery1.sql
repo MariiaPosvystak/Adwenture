f5bbd61b90865551fb76b7f10c662645d35fb1cb
Mariia Posvystak <mariiaposvystak@gmail.com>
2025-10-06 10:15:26 +0300

    lisa procedure Ajutise tabeli loomisega

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -66,3 +66,17 @@ select * from #PersonDetails
 --local temporary tabel on loodud?
 select name from tempdb..sysobjects
 where name like '#PersonDetails%'
+
+--lisa procedure Ajutise tabeli loomisega
+Create procedure spCreateLocalTempTable
+as
+begin
+CREATE TABLE #PersonDetails(Id INT, Name NVARCHAR(20));
+insert into #PersonDetails values(1, 'Mike');
+insert into #PersonDetails values(2, 'John');
+insert into #PersonDetails values(3, 'Todd');
+
+select * from #PersonDetails
+end;
+
+
